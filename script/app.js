1dd8683a8374b957fbe81deb80feae2bde5624c2
Saif <droconos99@gmail.com>
2023-09-22 18:43:28 +0600

    Partitioning code accross config files

--- script/app.js
+++ script/app.js
@@ -11,21 +11,7 @@ let computingMatrix = [
 
 const blockArray = document.querySelectorAll("#game-board li");
 const output = document.getElementById("output");
-
-for (let i = 0; i < 8; i++) {
-   for (let j = 0; j < 8; j++) {
-      setTimeout(() => {
-         blockArray[computingMatrix[i][j]].classList.add("snake");
-      }, 1000 * computingMatrix[i][j]);
-      setTimeout(() => {
-         if (j - 1 >= 0 && blockArray[computingMatrix[i][j - 1]].classList.contains("snake")) {
-            blockArray[computingMatrix[i][j - 1]].classList.remove("snake");
-         }
-      }, 1000 * computingMatrix[i][j]);
-   }
-   setTimeout(() => {
-      if (blockArray[computingMatrix[i][7]].classList.contains("snake")) {
-         blockArray[computingMatrix[i][7]].classList.remove("snake");
-      }
-   }, 1300 * computingMatrix[i][7 - i]);
-}
+const up = document.getElementById("up-button");
+const left = document.getElementById("left-button");
+const right = document.getElementById("right-button");
+const down = document.getElementById("down-button");
