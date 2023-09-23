12def91b0b0223c4a93c203fec1f700f182bed02
Saif <droconos99@gmail.com>
2023-09-23 11:58:31 +0600

    fixed bug related to all 4 direction of movements

--- script/config.js
+++ script/config.js
@@ -17,13 +17,14 @@
 // }
 
 function upMovement(event) {
+   let pointer = yAxis;
    for (let i = yAxis; i >= 0; i--) {
       setTimeout(() => {
          blockArray[computingMatrix[i][xAxis]].classList.add("snake");
          if (i < 7) {
             blockArray[computingMatrix[i + 1][xAxis]].classList.remove("snake");
          }
-      }, 200 * i);
+      }, 400 * (pointer - i));
       yAxis = i;
    }
 }
@@ -40,14 +41,16 @@ function downMovement(event) {
 }
 
 function leftMovement() {
+   let pointer = xAxis;
    for (let i = xAxis; i >= 0; i--) {
       setTimeout(() => {
          blockArray[computingMatrix[yAxis][i]].classList.add("snake");
-         if (i <= 7) {
+         if (i < 7) {
             blockArray[computingMatrix[yAxis][i + 1]].classList.remove("snake");
          }
-      }, 400 * i);
+      }, 400 * (pointer - i));
       xAxis = i;
+      console.log(xAxis);
    }
 }
 function rightMovement() {
