066150cc76bfb74464fd49cff4d7387ae3355631
Saif <droconos99@gmail.com>
2023-09-24 18:42:37 +0600

    reshaping game board

--- attempt-2/input..js
+++ attempt-2/input..js
@@ -1,5 +1,6 @@
 export let inputDirection = { x: 0, y: 0 };
 export let lastInputDirection = { x: 0, y: 0 };
+
 window.addEventListener("keydown", (event) => {
    switch (event.key) {
       case "ArrowUp":
